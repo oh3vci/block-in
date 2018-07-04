27f6bc7a4aea3f50c15f77d3ad5aa8f819bd249e
oh3vci <oh3vci@gmail.com>
2018-07-04 19:58:17 +0900

    MOD: Complete SelectDevices page without device picture

--- src/layouts/SelectDevicesPage.vue
+++ src/layouts/SelectDevicesPage.vue
@@ -1,4 +1,136 @@
 <template>
+  <v-app>
+    <v-toolbar
+      color="primary"
+      dark
+      flat
+    >
+      <v-btn icon>
+        <v-icon>fingerprint</v-icon>
+      </v-btn>
+      <v-toolbar-title>BLOCK - IN</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn icon>
+          <v-icon>close</v-icon>
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+
+    <v-list three-line>
+      <v-list-title>
+        <div class="text-primary section--head">Owner</div>
+        <v-layout row class="flex">
+          <v-flex xs2>
+            <v-avatar color="red">
+              <span class="white--text headline">J</span>
+            </v-avatar>
+          </v-flex>
+          <v-flex xs10>
+            <div class="describe">
+              <div class="headline">Jennie Kim</div>
+              <div>She was born in Anyang, a city in the province of Gyeong</div>
+            </div>
+          </v-flex>
+        </v-layout>
+      </v-list-title>
+      <v-divider></v-divider>
+      <v-list-title>
+        <v-flex xs12 column>
+          <div class="text-primary section--head">Date</div>
+          <div class="describe">
+            <div>18.06.24 10:00 AM ~ 18.06.26 15:00 PM</div>
+          </div>
+        </v-flex>
+      </v-list-title>
+      <v-divider></v-divider>
+      <v-list-title>
+        <v-flex xs12 column>
+          <div class="text-primary section--head">IoT Devices</div>
+          <v-layout row wrap class="device-wrapper">
+
+            <v-flex>
+              <v-card>
+                <v-container fluid fill-height pa-2>
+                  <v-layout fill-height>
+                    <v-flex xs12 align-end flexbox>
+                      <span>Air Conditioner</span>
+                      <div class="price">$3.0</div>
+                    </v-flex>
+                  </v-layout>
+                </v-container>
+              </v-card>
+            </v-flex>
+
+            <v-flex>
+              <v-card>
+                <v-container fluid fill-height pa-2>
+                  <v-layout>
+                    <v-flex xs12 align-end flexbox>
+                      <span>Door Lock</span>
+                      <div class="price">$3.0</div>
+                    </v-flex>
+                  </v-layout>
+                </v-container>
+              </v-card>
+            </v-flex>
+
+          </v-layout>
+
+          <v-layout row wrap class="device-wrapper">
+
+            <v-flex>
+              <v-card>
+                <v-container fluid fill-height pa-2>
+                  <v-layout fill-height>
+                    <v-flex xs12 align-end flexbox>
+                      <span>Air Conditioner</span>
+                      <div class="price">$3.0</div>
+                    </v-flex>
+                  </v-layout>
+                </v-container>
+              </v-card>
+            </v-flex>
+
+            <v-flex>
+              <v-card>
+                <v-container fluid fill-height pa-2>
+                  <v-layout>
+                    <v-flex xs12 align-end flexbox>
+                      <span>Door Lock</span>
+                      <div class="price">$3.0</div>
+                    </v-flex>
+                  </v-layout>
+                </v-container>
+              </v-card>
+            </v-flex>
+
+          </v-layout>
+
+        </v-flex>
+      </v-list-title>
+    </v-list>
+
+    <v-footer
+      color="primary"
+      app
+      fixed
+    >
+      <v-fab-transition>
+        <v-btn
+          color="emphasis"
+          dark
+          absolute
+          top
+          right
+          fab
+          @click="goNextPage()"
+        >
+          <v-icon>keyboard_arrow_right</v-icon>
+        </v-btn>
+      </v-fab-transition>
+    </v-footer>
+  </v-app>
 </template>
 
 
@@ -7,3 +139,67 @@ export default {
   name: 'SelectDevicesPage',
 };
 </script>
+
+
+<style scoped>
+.v-toolbar, .v-footer {
+  height: 8.75% !important;
+}
+.v-toolbar__title:not(:first-child) {
+  margin-left: 0;
+}
+.v-btn--floating {
+  border: 5px solid #fbfbfb !important;
+  box-shadow: none;
+  width: 64px;
+  height: 64px;
+}
+.v-btn--floating .v-btn__content i {
+  margin-top: 24px;
+}
+.section--head {
+  font-weight: bold;
+  font-size: 18px;
+}
+.describe {
+  display: inline-block;
+  margin-left: 8px;
+}
+.v-toolbar__content {
+  height: 100% !important;
+}
+.text-primary {
+  color: #6200ee;
+}
+.v-avatar {
+  vertical-align: top !important;
+}
+.v-btn--floating {
+  border: 5px solid #fbfbfb !important;
+  box-shadow: none;
+  width: 64px;
+  height: 64px;
+}
+.v-btn--floating .v-btn__content i {
+  margin-top: 24px;
+}
+v-list-title > .flex {
+  padding: 16px !important;
+}
+v-list-title > .text-primary {
+  padding-left: 16px;
+}
+.device-wrapper {
+  margin-bottom: 20px;
+}
+.device-wrapper .flex:first-child {
+  padding-right: 8px;
+}
+.device-wrapper .flex:last-child {
+  padding-left: 8px;
+}
+.price {
+  font-weight: bold;
+  font-size: 24px;
+}
+</style>
