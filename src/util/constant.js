3baf5351d18e48bca5c05beed9d92a86bcee3250
ysKim <znxkznxk1030@gmail.com>
2018-07-05 03:07:08 +0900

    [MOD] get deposit DATA

--- src/util/constant.js
+++ src/util/constant.js
@@ -3,4 +3,15 @@ export const SERVER_URL = 'http://blockin.iptime.org:1356';
 
 export const HDAC_CONTRACT = 'hdac';
 
-export const USER_ACCOUNT = '0x66d8F105DD5076986c7862181A75F46955b685A7';
+export const USER_ACCOUNT = '0x66d8f105dd5076986c7862181a75f46955b685a7';
+export const ACCOUNTS = [
+    "0x66d8f105dd5076986c7862181a75f46955b685a7", 
+    "0xa4928f50c77ea55b1bb9346c6d574d548a2d3ea9", 
+    "0x4f7ee8eb3a9d7b15dd94d737cc275f37bc329a02", 
+    "0xce9179bcb92850b8ad23cc349cb465874b70b4f2", 
+    "0x5539b5d81bb3cdd8528224ab98bfaf3219f3d2ac", 
+    "0x73d9a929e98e0dab8d61a89512d25513f54d21ca", 
+    "0xcfea70677f768e60caf91cda8aa77f2304e53ec8", 
+    "0xbad6f8c2102fd68bdd427dd3452df70496099be8", 
+    "0xc172c86fcefa2a3e81930a80fed1a9db439cf7ee"
+]
