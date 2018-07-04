a306f9883f8c39311f404baefcd4bf860c85e09c
oh3vci <oh3vci@gmail.com>
2018-07-04 23:08:02 +0900

    MOD: Eliminate alert top border

--- src/layouts/ConfirmPage.vue
+++ src/layouts/ConfirmPage.vue
@@ -138,5 +138,12 @@ export default {
 .v-alert {
   margin: 16px;
   font-size: 24px;
+  box-shadow:
+    0 3px 5px -1px rgba(0, 0, 0, .2),
+    0 6px 10px 0 rgba(0, 0, 0, .14),
+    0 1px 18px 0 rgba(0, 0, 0, .12);
+}
+.v-alert {
+  border: none !important;
 }
 </style>
