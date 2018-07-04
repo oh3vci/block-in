b675c1d223a5d5ce01fb83c346837a6b437212d3
oh3vci <oh3vci@gmail.com>
2018-07-04 16:08:23 +0900

    MOD: Complete MyPage layout

--- src/layouts/MyPage.vue
+++ src/layouts/MyPage.vue
@@ -3,44 +3,124 @@
     <v-toolbar
       color="primary"
       dark
-      fixed
     >
       <v-btn icon>
         <v-icon>fingerprint</v-icon>
       </v-btn>
       <v-toolbar-title>BLOCK - IN</v-toolbar-title>
     </v-toolbar>
-    <v-content>
-      <v-container fluid fill-height>
-        <v-layout
-          justify-center
-          align-center
-        >
-          <v-flex text-xs-center>
-            <v-card>
-              <v-card-title>
-                <div class="headline">JAEHO SUNG</div>
-              </v-card-title>
-            </v-card>
-          </v-flex>
-        </v-layout>
-      </v-container>
-      <v-btn
-        color="emphasis"
-        dark
-        absolute
-        bottom
-        right
-        fab
+
+    <v-container fluid grid-list-lg>
+      <v-layout
+        row
+        wrap
       >
-        <v-icon>add</v-icon>
-      </v-btn>
-    </v-content>
+        <v-flex xs12>
+          <v-card color="white" class="dark--text">
+            <v-container fluid grid-list-lg>
+              <v-layout>
+                <v-flex xs12 column>
+                  <div>
+                    <div class="headline customer">JAEHO SUNG</div>
+                    <div>user</div>
+                  </div>
+                </v-flex>
+              </v-layout>
+              <v-layout>
+                <v-flex xs8>
+                  <div class="text-primary balance">Balance</div>
+                  <div class="balance-amount">$70.50</div>
+                </v-flex>
+                <v-flex xs4>
+                  <v-btn color="primary">CHARGE</v-btn>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-card>
+        </v-flex>
+
+        <v-flex xs12>
+          <div class="section--head">Previous Contracts</div>
+        </v-flex>
+
+        <v-flex xs12>
+          <v-card color="white" class="dark--text">
+            <v-container fluid grid-list-lg>
+              <v-layout>
+                <v-flex xs10 column>
+                  <v-avatar>
+                    <img
+                      src="https://s3.amazonaws.com/vuetify-docs/images/john.jpg"
+                      alt="John"
+                    >
+                  </v-avatar>
+                  <div class="describe">
+                    <div class="headline">Jennie Kim</div>
+                    <div>18.06.24 ~ 18.06.26</div>
+                  </div>
+                </v-flex>
+                <v-flex xs2>
+                  <v-icon medium>share</v-icon>
+                </v-flex>
+              </v-layout>
+              <v-layout>
+                <v-flex xs12 class="total-price">
+                  <div>Total: $10.00</div>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-card>
+        </v-flex>
+
+        <v-flex xs12>
+          <v-card color="white" class="dark--text">
+            <v-container fluid grid-list-lg>
+              <v-layout>
+                <v-flex xs10 column>
+                  <v-avatar>
+                    <img
+                      src="https://s3.amazonaws.com/vuetify-docs/images/john.jpg"
+                      alt="John"
+                    >
+                  </v-avatar>
+                  <div class="describe">
+                    <div class="headline">Jennie Kim</div>
+                    <div>18.06.24 ~ 18.06.26</div>
+                  </div>
+                </v-flex>
+                <v-flex xs2>
+                  <v-icon medium>share</v-icon>
+                </v-flex>
+              </v-layout>
+              <v-layout>
+                <v-flex xs12 class="total-price">
+                  <div>Total: $10.00</div>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-card>
+        </v-flex>
+
+      </v-layout>
+    </v-container>
+
     <v-footer
       color="primary"
       app
       fixed
     >
+      <v-fab-transition>
+        <v-btn
+          color="emphasis"
+          dark
+          absolute
+          top
+          right
+          fab
+        >
+          <v-icon>add</v-icon>
+        </v-btn>
+      </v-fab-transition>
     </v-footer>
   </v-app>
 </template>
@@ -63,14 +143,49 @@ export default {
 .v-toolbar__title:not(:first-child) {
   margin-left: 0;
 }
+.v-avatar {
+  vertical-align: top !important;
+}
 .v-btn--floating {
-  border: 5px solid white !important;
+  border: 5px solid #fbfbfb !important;
   box-shadow: none;
+  width: 64px;
+  height: 64px;
+}
+.v-btn__content i {
+  margin-top: 24px;
+}
+.balance {
+  font-weight: bold;
+  font-size: 16px;
+}
+.balance-amount {
+  font-size: 48px;
+}
+.total-price > div {
+  text-align: center;
+  vertical-align: middle;
+  font-size: 24px;
+  font-weight: bold;
+}
+.customer {
+  font-weight: bold;
 }
 </style>
 
 <style>
+.section--head {
+  font-weight: bold;
+  font-size: 18px;
+}
+.describe {
+  display: inline-block;
+  margin-left: 8px;
+}
 .v-toolbar__content {
   height: 100% !important;
+}
+.text-primary {
+  color: #6200ee;
 }
 </style>
