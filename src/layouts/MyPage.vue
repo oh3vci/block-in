2507258af3d42e9317774cf020c2f1719e0f8f50
oh3vci <oh3vci@gmail.com>
2018-07-05 01:33:55 +0900

    MOD: Add usage card view after contraction confirmed

--- src/layouts/MyPage.vue
+++ src/layouts/MyPage.vue
@@ -40,6 +40,80 @@
           </v-card>
         </v-flex>
 
+        <v-flex v-if="isCheckedin" xs12>
+          <div class="section--head">Usage</div>
+        </v-flex>
+
+        <v-flex v-if="isCheckedin" xs12>
+          <v-card color="white" class="dark--text">
+            <v-container fluid grid-list-lg>
+              <v-layout>
+                <v-flex xs10 column>
+                  <v-avatar>
+                    <img
+                      src="https://s3.amazonaws.com/vuetify-docs/images/john.jpg"
+                      alt="John"
+                    >
+                  </v-avatar>
+                  <div class="describe">
+                    <div class="headline">Jennie Kim</div>
+                    <div>18.06.24 ~ 18.06.26</div>
+                  </div>
+                </v-flex>
+                <v-flex class="charge-button" xs4>
+                  <v-btn color="primary">LEAVE</v-btn>
+                </v-flex>
+              </v-layout>
+              <v-layout>
+                <div class="text-primary section--deposit">Deposit : $20.00</div>
+              </v-layout>
+            </v-container>
+
+            <v-layout row wrap class="device-wrapper">
+              <v-flex>
+                <v-card>
+                  <v-container fluid grid-list-lg>
+                    <v-layout>
+                      <v-flex xs12 flexbox>
+                        <span>Air Conditioner</span>
+                        <div class="price">$3.0</div>
+                      </v-flex>
+                    </v-layout>
+                    <v-layout>
+                      <v-flex xs12 flexbox>
+                        <v-btn block color="primary" dark>
+                          <v-icon>share</v-icon>
+                        </v-btn>
+                      </v-flex>
+                    </v-layout>
+                  </v-container>
+                </v-card>
+              </v-flex>
+
+              <v-flex>
+                <v-card>
+                  <v-container fluid grid-list-lg>
+                    <v-layout>
+                      <v-flex xs12 flexbox>
+                        <span>Door Lock</span>
+                        <div class="price">$3.0</div>
+                      </v-flex>
+                    </v-layout>
+                    <v-layout>
+                      <v-flex xs12 flexbox>
+                        <v-btn block color="primary" dark>
+                          <v-icon>share</v-icon>
+                        </v-btn>
+                      </v-flex>
+                    </v-layout>
+                  </v-container>
+                </v-card>
+              </v-flex>
+            </v-layout>
+            
+          </v-card>
+        </v-flex>
+
         <v-flex xs12>
           <div class="section--head">Previous Contracts</div>
         </v-flex>
@@ -144,6 +218,7 @@ export default {
       deposit: 0,
       totalPrice: 0,
       phone: '',
+      isCheckedin: false,
     };
   },
   mounted() {
@@ -160,6 +235,7 @@ export default {
       this.deposit = data._deposit;
       this.phone = data._phone;
       this.totalPrice = data._totalPrice;
+      this.isCheckedin = data._isCheckedin;
     });
   },
 };
@@ -172,5 +248,27 @@ export default {
 }
 .customer {
   font-weight: bold;
+}
+.section--deposit {
+  font-weight: bold;
+  font-size: 18px;
+  margin-left: 8px;
+}
+.device-wrapper .flex:first-child {
+  padding-right: 4px;
+  margin-left: 8px;
+}
+.device-wrapper .flex:last-child {
+  padding-left: 4px;
+  margin-right: 8px;
+}
+.device-wrapper .container {
+  padding: 8px !important;
+}
+.device-wrapper {
+  padding-bottom: 8px;
+}
+.v-card {
+  border: solid 1px rgba(0, 0, 0, 0.12);
 }
 </style>
